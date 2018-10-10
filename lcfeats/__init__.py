26d5acd843e689ab3b502e3fcf691ba88f95f5d2
R. Biswas <rbiswas4@gmail.com>
2018-10-10 14:59:30 -0600

    Adding missing files 	new file:   __init__.py 	new file:   version.py

---
+++ lcfeats/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+import os
+from .version import __VERSION__ as __version__
+
+here = __file__
+basedir = os.path.split(here)[0]
+# data = os.path.join(basedir, 'data')
+example_data = os.path.join(basedir, 'example_data')
+from .add_features import *
+
+
