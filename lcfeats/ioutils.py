5e2d1c20de2740e8bd2ce2d7e10af5e9be3c73df
R. Biswas <rbiswas4@gmail.com>
2018-11-17 21:15:07 +0100

    Added SNR 	modified:   ../examples/fromSNANA.ipynb 	modified:   ioutils.py 	modified:   version.py

--- lcfeats/ioutils.py
+++ lcfeats/ioutils.py
@@ -11,14 +11,17 @@ import pandas as pd
 def get_rootfile(headname):
         return os.path.split(headname)[1].split('_HEAD')[0]
 
-def get_photTable(snsims):
+def get_photTable(snsims, filter_query='SNID%50==0'):
     """
     Instance of `SNANASims`
     """
+    snids = snsims.headData.reset_index().query(filter_query).SNID
     lcs = []
-    for snid in snsims.headData.reset_index().SNID:
+    for snid in snids:
         lcdata = snsims.get_SNANA_photometry(snid, keepSnid=True).lightCurve
         lcs.append(lcdata)
-        
-    return pd.concat(lcs)
+    # Need something if snid is None: 
+    df = pd.concat(lcs)
+    df['SNR'] = df.flux / df.fluxerr
+    return df 
 
